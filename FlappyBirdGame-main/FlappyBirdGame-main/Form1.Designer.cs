1b28b347dba9e0f9cce919471e16bb52e8494d96
recepcosar <recep.csr21@gmail.com>
2025-10-01 23:19:01 +0300

    Add files via upload

---
+++ FlappyBirdGame-main/FlappyBirdGame-main/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.üst = new System.Windows.Forms.PictureBox();
+            this.zemin = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.alt = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.üst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // üst
+            // 
+            this.üst.Image = ((System.Drawing.Image)(resources.GetObject("üst.Image")));
+            this.üst.Location = new System.Drawing.Point(694, -2);
+            this.üst.Name = "üst";
+            this.üst.Size = new System.Drawing.Size(91, 128);
+            this.üst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.üst.TabIndex = 0;
+            this.üst.TabStop = false;
+            // 
+            // zemin
+            // 
+            this.zemin.Image = ((System.Drawing.Image)(resources.GetObject("zemin.Image")));
+            this.zemin.Location = new System.Drawing.Point(2, 461);
+            this.zemin.Name = "zemin";
+            this.zemin.Size = new System.Drawing.Size(950, 58);
+            this.zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.zemin.TabIndex = 2;
+            this.zemin.TabStop = false;
+            // 
+            // bird
+            // 
+            this.bird.Image = ((System.Drawing.Image)(resources.GetObject("bird.Image")));
+            this.bird.Location = new System.Drawing.Point(286, 160);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(80, 60);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bird.TabIndex = 3;
+            this.bird.TabStop = false;
+            // 
+            // alt
+            // 
+            this.alt.Image = ((System.Drawing.Image)(resources.GetObject("alt.Image")));
+            this.alt.Location = new System.Drawing.Point(568, 337);
+            this.alt.Name = "alt";
+            this.alt.Size = new System.Drawing.Size(101, 127);
+            this.alt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.alt.TabIndex = 4;
+            this.alt.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.gameTimer);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 39);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Scoure:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(956, 571);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.alt);
+            this.Controls.Add(this.bird);
+            this.Controls.Add(this.zemin);
+            this.Controls.Add(this.üst);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.game_Down);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.game_Up);
+            ((System.ComponentModel.ISupportInitialize)(this.üst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox üst;
+        private System.Windows.Forms.PictureBox zemin;
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox alt;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FlappyBirdGame-main/FlappyBirdGame-main/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+
+        int pipeSpeed = 8;
+        int gravity = 10;
+        int scoure = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimer(object sender, EventArgs e)
+        {
+            bird.Top += gravity;
+            alt.Left -= pipeSpeed;
+            üst.Left -= pipeSpeed;
+            label1.Text = "SCORE:" + scoure;
+
+            if (alt.Left < -150)
+            {
+                alt.Left = 800;
+                scoure++;
+            }
+
+            if (üst.Left < -180)
+            {
+                üst.Left = 950;
+                scoure++;
+            }
+
+            if (bird.Bounds.IntersectsWith(alt.Bounds) ||
+                bird.Bounds.IntersectsWith(üst.Bounds) ||
+                bird.Bounds.IntersectsWith(zemin.Bounds) || bird.Top < -25)
+            {
+                endGame();
+            }
+        }
+
+        public void endGame()
+        {
+            timer1.Stop();
+            label1.Text = "GAME OVER !!!";
+        }
+
+        private void game_Down(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10;
+            }
+        }
+
+        private void game_Up(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 10;
+            }
+        }
+    }
+}
